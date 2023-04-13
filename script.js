81ad4b9c7fb28ee083e64e1c6ef66c931ee77605
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-04-13 22:17:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,4 +12,28 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
-// You can write your code here
+// Function to measure the time taken for a promise to resolve
+async function measureTime(promise) {
+  const start = performance.now();
+  await promise;
+  const end = performance.now();
+  return end - start;
+}
+
+// Fetch data from all APIs using Promise.all() and measure the time taken
+Promise.all(apiUrls.map(url => fetch(url)))
+  .then(responses => {
+    const totalTime = responses.reduce((acc, response) => {
+      return acc + measureTime(response.json());
+    }, 0);
+    document.getElementById("output-all").textContent = totalTime.toFixed(2);
+  })
+  .catch(error => console.error(error));
+
+// Fetch data from any API using Promise.any() and measure the time taken
+Promise.any(apiUrls.map(url => fetch(url).then(response => response.json())))
+  .then(data => measureTime(Promise.resolve(data)))
+  .then(time => {
+    document.getElementById("output-any").textContent = time.toFixed(2);
+  })
+  .catch(error => console.error(error));
